e6eca414b112ca1fe0343528b37ae59fd9093998
Christoph Gleinser <gc0453@mci4me.at>
2026-05-29 13:38:45 +0200

    Dataframe mit Leistungswerten

--- power_curve.py
+++ power_curve.py
@@ -5,20 +5,26 @@ def power_curve(data):
     power_curve = []
     power_curve = data["PowerOriginal"]
 
-    #zeit = [1, 3, 6, 10, 30, 60, 100, 300, 600, 1000, 3000, 6000]
+    zeit = [1, 3, 6, 10, 30, 60, 100, 300, 600, 1000, 3000, 6000]
     #i = len(zeit)
+    print(data["PowerOriginal"].dtype)
 
     #i = len(power_curve)
     i = 0
-    pw = []
-    for i in range(i,len(power_curve)):
-        for j in range(i + 1,len(power_curve)):
-            window = power_curve.iloc[i:j]
+    power_result = []
+    for dauer in zeit:
+        max_power = 0
+        for i in range(len(power_curve) - dauer + 1):
+            window = power_curve.iloc[i:i + dauer]
             avg_power = window.mean()
-            pw.append(avg_power)
+
+            if avg_power > max_power:
+                max_power = avg_power
+        power_result.append(max_power)
 
 
-    return pw
+
+    return print(power_result)
 
 
 power_curve(df_csv())
